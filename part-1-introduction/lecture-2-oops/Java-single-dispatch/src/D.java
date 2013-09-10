978606418c2f81466b3822929755201a709a83aa
Guido Wachsmuth <g.h.wachsmuth@tudelft.nl>
2013-09-10 15:38:36 +0200

    initial Java example for single dispatch

---
+++ part-1-introduction/lecture-2-oops/Java-single-dispatch/src/D.java
@@ -0,0 +1,4 @@
+public class D {
+	public A m(A a) { System.out.println("D.m(A a)"); return a; }
+	public A m(B b) { System.out.println("D.m(B b)"); return b; }
+}
---
+++ part-1-introduction/lecture-2-oops/Java-single-dispatch/src/E.java
@@ -0,0 +1,4 @@
+public class E extends D {
+	public A m(A a) { System.out.println("E.m(A a)"); return a; }
+	public B m(B b) { System.out.println("E.m(B b)"); return b; }
+}
