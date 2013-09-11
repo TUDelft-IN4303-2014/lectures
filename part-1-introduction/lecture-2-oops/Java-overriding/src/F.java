ec4b357390637c408e49d9b88146f28b2070b9e9
Guido Wachsmuth <g.h.wachsmuth@tudelft.nl>
2013-09-11 13:26:58 +0200

    combined Java examples and moved project

--- part-1-introduction/lecture-2-oops/Java-overriding/src/F.java
+++
@@ -1,3 +0,0 @@
-public class F {
-	public A m(B b) { System.out.println("F.m(B b)"); return b; }
-}
--- part-1-introduction/lecture-2-oops/Java-single-dispatch/src/D.java
+++
@@ -1,4 +0,0 @@
-public class D {
-	public A m(A a) { System.out.println("D.m(A a)"); return a; }
-	public A m(B b) { System.out.println("D.m(B b)"); return b; }
-}
